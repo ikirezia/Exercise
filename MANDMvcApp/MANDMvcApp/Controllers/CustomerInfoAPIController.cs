281599fed5cd19ddf91c2f1ce2897f67fc849de3
ikirezia <marieantoinette.dorikirezi@gmail.com>
2015-11-08 23:59:04 -0500

    MAnd

---
+++ MANDMvcApp/MANDMvcApp/Controllers/CustomerInfoAPIController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using MANDMvcApp.Models;
+
+namespace MANDMvcApp.Controllers
+{
+    public class CustomerInfoAPIController : ApiController
+    {
+        private YleanaDatabaseEntities db = new YleanaDatabaseEntities();
+
+        // GET api/CustomerInfoAPI
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return db.Customers.AsEnumerable();
+        }
+
+        // GET api/CustomerInfoAPI/5
+        public Customer GetCustomer(int id)
+        {
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return customer;
+        }
+
+        // PUT api/CustomerInfoAPI/5
+        public HttpResponseMessage PutCustomer(int id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+            if (id != customer.CustomerID)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            db.Entry(customer).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        // POST api/CustomerInfoAPI
+        public HttpResponseMessage PostCustomer(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(customer);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, customer);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = customer.CustomerID }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+
+        // DELETE api/CustomerInfoAPI/5
+        public HttpResponseMessage DeleteCustomer(int id)
+        {
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.Customers.Remove(customer);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, customer);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
